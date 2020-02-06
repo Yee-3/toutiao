9f7d70c7f72c1c22fbb54a5be0bccb60b94960ee
yee-3 <1431796149@qq.com>
2020-02-06 13:40:44 +0800

    login页面背景及小窗口

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <router-view></router-view>
     <!-- 空 -->
-    <el-button type="success">成功按钮</el-button>
+    <!-- <el-button type="success">成功按钮</el-button> -->
   </div>
 </template>
 
