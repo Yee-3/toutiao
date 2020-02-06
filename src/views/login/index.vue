50165600e62f2e1c85b82ed0faf2f26835f01e3f
yee-3 <1431796149@qq.com>
2020-02-06 16:43:41 +0800

    校验功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,39 +3,83 @@
   <!-- 全屏容器 -->
   <div class="container-login">
     <el-card class="my-card">
-      <img src="../../assets/logo_index.png" alt="">
+      <img src="../../assets/logo_index.png" alt />
+      <!-- 表单 -->
+      <el-form :model="loginForm" :rules="loginRules" status-icon>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px;margin-right:8px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
- name: 'app-login',
-}
+  name: "app-login",
+  data() {
+    const checkMobile=(rule,value,callback)=>{
+      if(!/^1[3-9]\d{9}$/.test(value)){
+        // 校验失败
+        callback(new Error('手机号格式错误'))
+      }else{
+        // 校验成功
+        callback()
+      }
+    }
+    return {
+      //  表单对应的数据对象
+      loginForm: {
+        mobile: "",
+        code: ""
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          {validator:checkMobile,trigger:'blur'}
+          ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          {len:6,message:'验证码6个字符',trigger:'blur'}
+          ]
+      }
+    };
+  }
+};
 </script>
 
 <style scoped lang='less'>
-.container-login{
-  width:100%;
+.container-login {
+  width: 100%;
   height: 100%;
   position: absolute;
-  top:0;
+  top: 0;
   left: 0;
   background: url(../../assets/login_bg.jpg) no-repeat;
   background-size: cover;
-  .my-card{
-    width:400px;
+  .my-card {
+    width: 400px;
     height: 350px;
-    transform: translate(-50%,-60%);
+    transform: translate(-50%, -60%);
     position: absolute;
     left: 50%;
-    top:50%;
-    img{
+    top: 50%;
+    img {
       width: 200px;
       display: block;
-      margin: 0 auto 20px
+      margin: 0 auto 20px;
     }
   }
-
 }
 </style>
