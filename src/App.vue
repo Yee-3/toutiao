94fe503c3daebbcbd2aa626df90c5f9cea7bc136
yee-3 <1431796149@qq.com>
2020-02-06 11:08:58 +0800

    login页面

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <router-view></router-view>
     <!-- 空 -->
     <el-button type="success">成功按钮</el-button>
   </div>
---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="container">Login</div>
+</template>
+
+<script>
+export default {
+  name: 'app-login',
+  
+}
+</script>
+
+<style lang="less">
+
+</style>
