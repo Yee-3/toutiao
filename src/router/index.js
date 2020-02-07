12b8a2a6cc6e8251fc3503726a8921c2a22b6df2
yee-3 <1431796149@qq.com>
2020-02-07 17:27:18 +0800

    404页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import Login from '@/views/login'
 import Home from '@/views/home'
 import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
@@ -10,9 +11,10 @@ const router = new VueRouter({
     {
       path: '/', component: Home,
       children: [
-        {path:'/',component:Welcome}
+        { path: '/', component: Welcome }
       ]
-  }
+    },
+    { path: '*', component: NotFound }
   ]
 })
 export default router
