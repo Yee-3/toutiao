3d3ce61d00c8670693b578005761ccd273ebb168
yee-3 <1431796149@qq.com>
2020-02-10 15:56:10 +0800

    解决token失效问题

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 // 导出一个配置好的axios提供给main使用
 import axios from 'axios'
 import auth from '@/utils/auth'
+import router from '@/router'
 // 配置axios
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 // axios.defaults.headers.Authorization=`Bearer ${auth.getUser().token}`
@@ -9,7 +10,25 @@ axios.interceptors.request.use(config => {
   if (user.token) config.headers.Authorization = `Bearer ${user.token}`
   return config;
 }, error => {
-    return Promise.reject(error)
+  return Promise.reject(error)
+})
+// 响应拦截器
+axios.interceptors.response.use(res => {
+  return res
+}, err => {
+  // token失效，后台会报错
+  // err.response响应报文对象
+  // err.response.status 状态码
+    if (err.response && err.response.status === 401) {
+    // 拦截到登录
+      // 大家代码：this.$router.push('/login')
+      // this  一定不是vue实例，只有vue实例才能调用 $router或$route
+      // $router就是路由实例，我们在route/index.js初始化过理由实例
+      // $router===router/index.js
+      // window.location.href这样跳转可能不会触发路由的相关功能
+    router.push('./login')
+  }
+  return Promise.reject(err)
 })
 
 // 导出
