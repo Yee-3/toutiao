ea7179b0014cb080bfbae17d469d762c886bce2f
yee-3 <1431796149@qq.com>
2020-02-10 11:49:59 +0800

    axios配置

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -8,7 +8,7 @@
 export default {
   name:"app-article",
   created(){
-    this.$http.get('http://ttapi.research.itcast.cn/mp/v1_0/articles').then(res=>{
+    this.$http.get('articles').then(res=>{
       console.log(res.data);
       
     }).catch(err=>{
