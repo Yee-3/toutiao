20e222f16d68e085447f0627975f63bde44d7f38
yee-3 <1431796149@qq.com>
2020-02-11 11:19:56 +0800

    面包屑和表单结构

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,22 +1,40 @@
 <template>
-<div class='container-article'>
-  article
-</div>
+  <div class="container-article">
+    <!-- 筛选区域 -->
+    <el-card>
+      <!-- 面包屑 -->
+      <div class="header">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <!-- :to="{ path: '/' }" 和 router-link to 属性一致  -->
+          <!-- { path: '/' } 使用对象和  to="/"  作用一致 -->
+          <el-breadcrumb-item :to="{path:'/'}">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-form label-width="80px">
+        <el-form-item label="状态"></el-form-item>
+        <el-form-item label="频道"></el-form-item>
+        <el-form-item label="日期"></el-form-item>
+        <el-form-item></el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name:"app-article",
-  created(){
-    this.$http.get('articles').then(res=>{
-      console.log(res.data);
-      
-    }).catch(err=>{
-      console.log(err);
-      
-    })
-  }
-}
+  name: "app-article",
+  // created() {
+  //   this.$http
+  //     .get("articles")
+  //     .then(res => {
+  //       console.log(res.data);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
+};
 </script>
 
 <style scoped lang='less'></style>
