e810063d5f82749096797c447d8cedd29a77965c
yee-3 <1431796149@qq.com>
2020-02-11 11:31:50 +0800

    内容管理-频道&日期按钮

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -3,7 +3,7 @@
     <!-- 筛选区域 -->
     <el-card>
       <!-- 面包屑 -->
-      <div class="header">
+      <div slot="header">
         <el-breadcrumb separator-class="el-icon-arrow-right">
           <!-- :to="{ path: '/' }" 和 router-link to 属性一致  -->
           <!-- { path: '/' } 使用对象和  to="/"  作用一致 -->
@@ -11,7 +11,7 @@
           <el-breadcrumb-item>内容管理</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
-      <el-form label-width="80px">
+      <el-form label-width="80px" size="small">
         <el-form-item label="状态">
           <el-radio-group v-model="filterData.status">
             <el-radio :label="null">全部</el-radio>
@@ -22,9 +22,29 @@
             <el-radio :label="4">已删除</el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="频道"></el-form-item>
-        <el-form-item label="日期"></el-form-item>
-        <el-form-item></el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="filterData.channel_id" placeholder="请选择">
+            <el-option
+              v-for="item in channelOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+            <!-- label 选项文字  value 选项的值 当你选择某个选项后，该选项的值提供v-model -->
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期">
+          <el-date-picker
+            v-model="dateArr"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">筛选</el-button>
+        </el-form-item>
       </el-form>
     </el-card>
   </div>
@@ -33,12 +53,22 @@
 <script>
 export default {
   name: "app-article",
-  data(){
-    return{
-      filterData:{
-        status:null
-      }
-    }
+  data() {
+    return {
+      filterData: {
+        status: null,
+        channel_id: null,
+        begin_pubdate: null,
+        end_pubdate: null
+      },
+      channelOptions: [
+        { label: "前端", value: 1 },
+        { label: "数据库", value: 2 }
+      ],
+      // 日期范围数据 [起始日期,结束日期]
+      // 但是选择完成日期范围后，可以根据这个数据给 begin_pubdate end_pubdate 赋值。
+      dateArr: []
+    };
   }
   // created() {
   //   this.$http
