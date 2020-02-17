06fd455cb45bed8f847d77387c8f45c46439233b
yee-3 <1431796149@qq.com>
2020-02-17 09:52:11 +0800

    发布文章-封面组件-单选框组

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -11,7 +11,17 @@
         <el-form-item label="内容：">
           <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
         </el-form-item>
-        <el-form-item label="封面：">封面组件</el-form-item>
+        <el-form-item label="封面：">
+          <el-radio-group v-model="articleForm.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
+          </el-radio-group>
+          <div>
+            <!-- 组件位置 -->
+          </div>
+        </el-form-item>
         <el-form-item label="频道：">
           <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
@@ -40,20 +50,26 @@ export default {
       articleForm: {
         title: null,
         channel_id: null,
-        content: null
+        content: null,
+        cover:{
+          // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          type: 1,
+          // 放图片地址
+          images: []
+        }
       },
-     editorOption: {
+      editorOption: {
         // 站位文字
-        placeholder: '',
+        placeholder: "",
         // 模板配置
-        modules:{
+        modules: {
           toolbar: [
-            ['bold', 'italic', 'underline', 'strike'],
-            ['blockquote', 'code'],
-            [{ 'header': 1 }, { 'header': 2 }],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            [{ 'indent': '-1' }, { 'indent': '+1' }],
-            ['image']
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code"],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            ["image"]
           ]
         }
       }
