7b1d7171ee0edb0e6adcd1a7317fab087051f55e
yee-3 <1431796149@qq.com>
2020-02-17 20:52:22 +0800

    修改

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -47,7 +47,6 @@ import "quill/dist/quill.bubble.css";
 import { quillEditor } from "vue-quill-editor";
 export default {
   name: "app-publish",
-  props: ['value'],
   components: { quillEditor },
   data() {
     return {
