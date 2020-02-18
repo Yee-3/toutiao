0066085b531f840887829a7e1e8f0aae0b0911b3
yee-3 <1431796149@qq.com>
2020-02-18 09:46:19 +0800

    封面组件-添加图片最后逻辑

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -12,18 +12,18 @@
           <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
         </el-form-item>
         <el-form-item label="封面：">
-          <el-radio-group v-model="articleForm.cover.type">
+          <el-radio-group @change="articleForm.cover.images=[]" v-model="articleForm.cover.type">
             <el-radio :label="1">单图</el-radio>
             <el-radio :label="3">三图</el-radio>
             <el-radio :label="0">无图</el-radio>
             <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
-          <div>
+          <div v-if="articleForm.cover.type===1">
             <!-- 组件位置 -->
             <my-image v-model="articleForm.cover.images[0]"></my-image>
-            <my-image></my-image>
-            <my-image></my-image>
-            <my-image></my-image>
+          </div>
+          <div v-if="articleForm.cover.type===3">
+            <my-image :key="i" v-for="i in 3" v-model="articleForm.cover.images[i-1]"></my-image>
           </div>
         </el-form-item>
         <el-form-item label="频道：">
@@ -55,7 +55,7 @@ export default {
         title: null,
         channel_id: null,
         content: null,
-        cover:{
+        cover: {
           // 封面类型 -1:自动，0-无图，1-1张，3-3张
           type: 1,
           // 放图片地址
@@ -68,12 +68,12 @@ export default {
         // 模板配置
         modules: {
           toolbar: [
-            ['bold', 'italic', 'underline', 'strike'],
-            ['blockquote', 'code'],
-            [{ 'header': 1 }, { 'header': 2 }],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            [{ 'indent': '-1' }, { 'indent': '+1' }],
-            ['image']
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code"],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            ["image"]
           ]
         }
       }
