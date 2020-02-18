f238625b3ece5a1a7e83cf387b90b78844d9bb90
yee-3 <1431796149@qq.com>
2020-02-18 12:29:49 +0800

    修改文章-渲染逻辑

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -2,7 +2,7 @@
   <div class="container-publish">
     <el-card>
       <div slot="header">
-        <my-bread>发布文章</my-bread>
+        <my-bread>{{$route.query.id?'修改':'发布'}}文章</my-bread>
       </div>
       <el-form label-width="120px">
         <el-form-item label="标题：">
@@ -29,7 +29,10 @@
         <el-form-item label="频道：">
           <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
-        <el-form-item>
+        <el-form-item v-if="$route.query.id">
+          <el-button type="success">修改文章</el-button>
+        </el-form-item>
+        <el-form-item v-else>
           <el-button @click="submit(false)" type="primary">发布文章</el-button>
           <el-button @click="submit(true)">存入草稿</el-button>
         </el-form-item>
@@ -79,18 +82,53 @@ export default {
       }
     };
   },
+  created () {
+    // 当修改文章的时候，获取文章信息填充表单
+    if (this.$route.query.id) {
+      this.getArticle();
+    }
+  },
+  watch:{
+    // 监听地址栏id的变化
+    '$route.query.id':function(){
+      this.toggleFormInfo()
+    }
+  },
   methods: {
+    // 切换表单内容
+    toggleFormInfo(){
+      if(this.$route.query.id){
+        // 修改
+       this.getArticle();
+      }else{
+        // 发布
+        this.articleForm={
+          title:null,
+          channel_id:null,
+          content:null ,
+          cover:{
+            type:1,
+            images:[]
+          }
+        }
+      }
+    },
+    // 获取文章信息
+    async getArticle(){
+      const res=await this.$http.get(`articles/${this.$route.query.id}`)
+      this.articleForm=res.data.data
+    },
     // 添加文章 draft===false 发布文章  draft===true  存入草稿
-    async submit (draft) {
+    async submit(draft) {
       try {
         // 请求
-        await this.$http.post(`articles?draft=${draft}`, this.articleForm)
+        await this.$http.post(`articles?draft=${draft}`, this.articleForm);
         // 提示
-        this.$message.success(draft ? '存入草稿成功' : '发布文章成功')
+        this.$message.success(draft ? "存入草稿成功" : "发布文章成功");
         // 跳转
-        this.$router.push('/article')
+        this.$router.push("/article");
       } catch (e) {
-        this.$message.error('操作失败')
+        this.$message.error("操作失败");
       }
     }
   }
