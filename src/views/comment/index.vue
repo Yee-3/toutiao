ab0e9700d7a8d6f2206a0844d41c8d596835fea8
yee-3 <1431796149@qq.com>
2020-02-18 17:17:05 +0800

    评论管理-切换状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -16,8 +16,8 @@
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="small" v-if="scope.row.comment_status" type="danger">关闭评论</el-button>
-            <el-button size="small" v-else type="success">打开评论</el-button>
+            <el-button @click="toggleStatus(scope.row)" size="small" v-if="scope.row.comment_status" type="danger">关闭评论</el-button>
+            <el-button @click="toggleStatus(scope.row)" size="small" v-else type="success">打开评论</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -56,6 +56,29 @@ export default {
     this.getComments()
   },
   methods: {
+    // 切换状态
+    toggleStatus(row){
+      const text1='您是否确定要关闭该文章的评论功能，关闭后用户无法对文章进行评论'
+      const text2='您是否确定打开该文章的评论功能'
+      this.$confirm(row.comment_status?text1:text2,'温馨提示',{
+        confirmButtonText:'确定',
+        cancelButtonText:'取消',
+        type:'warning'
+      }).then(async()=>{
+        try{
+          // 请求
+          const res=await this.$http.put(`comments/status?article_id=${row.id}`,{
+            allow_comment:!row.comment_status
+          })
+          // 提示
+          this.$message.success(res.data.data.allow_comment?'打开成功':'关闭成功')
+          // 修改当前文章的评论状态
+          row.comment_status=res.data.data.allow_comment
+        }catch (e) {
+          this.$message.error('操作失败');
+        }
+      }).catch(()=>{})
+    },
     // 分页
     pager (newPage) {
       this.reqParams.page = newPage
